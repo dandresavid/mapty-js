57c400bfb5540d77e607361c9fb772aaa020fb3c
Andres David Blandon <dandresavid@hotmail.com>
2022-07-21 15:58:43 -0500

    Added move to marker on click feature

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 class Workout{
     date = new Date();
     id = ( Date.now() + '').slice(-10);
+    clicks = 0;
 
     constructor(coords, distance, duration){
         // this.date =
@@ -17,6 +18,10 @@ class Workout{
 
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]
         } ${this.date.getDate()}`
+    }
+
+    _click() {
+        this.clicks++;
     }
 }
 
@@ -71,6 +76,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 class App{
     #map;
+    #mapZoomLevel = 13;
     #mapEvent;
     #workouts = [];
 
@@ -99,7 +105,7 @@ class App{
         const coord = [latitude,longitude];
 
         console.log(this);
-        this.#map = L.map('map').setView(coord, 13);
+        this.#map = L.map('map').setView(coord, this.#mapZoomLevel);
         //console.log(this);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -257,13 +263,22 @@ class App{
 
     _moveToPopup(e) {
          const workoutEl = e.target.closest('.workout')
-         console.log(workoutEl);
 
          if(!workoutEl) return;
 
-         const workout = this.#workouts.find(work => work.id === workoutEl.dataset.id);
-         console.log(workout);
+         const workout = this.#workouts.find(
+            work => work.id === workoutEl.dataset.id
+        );
 
+        this.#map.setView(workout.coord, this.#mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1,
+            }
+        });
+
+        //using the public interface
+        workout._click();
     }
 }
 
